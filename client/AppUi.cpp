9345d1ff7377ddb78719abb7883cbe5c5f665d97
Daryna <ddanyliuk@kse.org.ua>
2026-07-01 16:02:47 +0300

    faet: front-end development

---
+++ client/AppUi.cpp
@@ -0,0 +1,5 @@
+//
+// Created by KTROOM on 30.06.2026.
+//
+
+#include "AppUi.h"
---
+++ client/AppUi.h
@@ -0,0 +1,13 @@
+//
+// Created by KTROOM on 30.06.2026.
+//
+
+#ifndef COURSE_PROJECT_II_APPUI_H
+#define COURSE_PROJECT_II_APPUI_H
+
+
+class AppUi {
+};
+
+
+#endif //COURSE_PROJECT_II_APPUI_H
