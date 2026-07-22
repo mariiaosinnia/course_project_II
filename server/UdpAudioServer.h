f144fb15684263ad2900090b0f2453e084540c60
dburdak <dburdak@kse.org.ua>
2026-07-22 04:44:49 +0300

    modified files to be easy tested

--- server/UdpAudioServer.h
+++ server/UdpAudioServer.h
@@ -1,66 +1,74 @@
 #pragma once
-#include <string>
-#include <memory>
-#include <unordered_map>
-#include <mutex>
-#include <thread>
 #include <filesystem>
+#include <memory>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <unordered_map>
 
-#include "Protocol.h"
-#include "Track.h"
-#include "Mp3Decoder.h"
-#include "Resampler.h"
 #include "../shared/AudioEncoder.h"
-#include "UdpSocket.h"
 #include "../shared/MusicStreamer.h"
+#include "Mp3Decoder.h"
+#include "Protocol.h"
+#include "Resampler.h"
 #include "RoomManager.h"
+#include "Track.h"
+#include "UdpSocket.h"
+
+#include <optional>
+
+struct VoiceRoutingResult {
+  std::vector<uint8_t> packet;
+  std::vector<udp::endpoint> endpoints;
+};
 
 struct PlaybackRoom {
-    std::shared_ptr<Track> track;
-    size_t current_packet_index = 0;
-    uint32_t sequence_number = 0;
-    std::chrono::steady_clock::time_point next_send_time;
+  std::shared_ptr<Track> track;
+  size_t current_packet_index = 0;
+  uint32_t sequence_number = 0;
+  std::chrono::steady_clock::time_point next_send_time;
 };
 
 class UdpAudioServer {
 public:
-    UdpAudioServer(unsigned short udp_port, RoomManager& room_manager,
-                   std::filesystem::path resource_dir);
+  UdpAudioServer(unsigned short udp_port, RoomManager &room_manager,
+                 std::filesystem::path resource_dir);
 
-    uint16_t addTrack(const std::string& file_path);
+  uint16_t addTrack(const std::string &file_path);
 
-    void startStreaming(uint16_t room_id);
+  void startStreaming(uint16_t room_id);
 
-    void stopStreaming();
-    void stopRoomStreaming(uint16_t room_id);
+  void stopStreaming();
+  void stopRoomStreaming(uint16_t room_id);
 
-    void registerClient(uint32_t client_id, const udp::endpoint& endpoint);
+  void registerClient(uint32_t client_id, const udp::endpoint &endpoint);
 
-    void run();
-    std::vector<TrackListEntry> getTrackList() const;
-    bool isTrackExists(uint16_t track_id) const;
-
+  void run();
+  std::vector<TrackListEntry> getTrackList() const;
+  bool isTrackExists(uint16_t track_id) const;
 
 private:
-    void receiveLoop();
-    void schedulerLoop();
-    void loadDefaultTracks();
-    std::shared_ptr<Track> prepareTrack(const std::string& file_path);
-    void handleVoicePacket(const std::vector<uint8_t>& buffer, size_t bytes, const udp::endpoint& sender);
+  void receiveLoop();
+  void schedulerLoop();
+  void loadDefaultTracks();
+  std::shared_ptr<Track> prepareTrack(const std::string &file_path);
+  std::optional<VoiceRoutingResult>
+  handleVoicePacket(const std::vector<uint8_t> &buffer, size_t bytes,
+                    const udp::endpoint &sender);
 
-    RoomManager&    room_manager_;
-    UdpSocket       udp_socket_;
-    Mp3Decoder      decoder_;
-    Resampler       resampler_;
+  RoomManager &room_manager_;
+  UdpSocket udp_socket_;
+  Mp3Decoder decoder_;
+  Resampler resampler_;
 
-    std::unordered_map<uint16_t, std::shared_ptr<Track>> tracks_;
-    uint16_t next_track_id_ = 1;
-    std::mutex tracks_mutex_;
+  std::unordered_map<uint16_t, std::shared_ptr<Track>> tracks_;
+  uint16_t next_track_id_ = 1;
+  std::mutex tracks_mutex_;
 
-    std::unordered_map<uint16_t, PlaybackRoom> active_rooms_;
-    std::mutex active_rooms_mutex_;
+  std::unordered_map<uint16_t, PlaybackRoom> active_rooms_;
+  std::mutex active_rooms_mutex_;
 
-    std::filesystem::path resource_dir_;
-    std::atomic<bool> running {true};
-    std::thread scheduler_thread_;
+  std::filesystem::path resource_dir_;
+  std::atomic<bool> running{true};
+  std::thread scheduler_thread_;
 };
